d9a230192990eaecb36dd05088d86f9d900f6083
Usmael Abdureman <95717548+Usmaelabdureman@users.noreply.github.com>
2022-01-14 22:02:16 +0300

    first

--- src/main/java/com/project/gojjotest/GojjoTestApplication.java
+++ src/main/java/com/project/gojjotest/GojjoTestApplication.java
@@ -2,7 +2,6 @@ package com.project.gojjotest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class GojjoTestApplication {
 	public static void main(String[] args) {
