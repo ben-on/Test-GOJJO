9fae63228c297c84bfd9c65112ea7fa5108186be
Usmael Abdureman <95717548+Usmaelabdureman@users.noreply.github.com>
2022-01-16 11:12:55 +0300

    update

--- src/main/java/com/project/gojjotest/aboutController.java
+++ src/main/java/com/project/gojjotest/aboutController.java
@@ -2,16 +2,12 @@ package com.project.gojjotest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class aboutController {
-    @GetMapping("/aboutUs")
+    @GetMapping("/about")
     public String about(){
         return "aboutUs" ;
     }
-    @RequestMapping("/aboutUs")
-    public String Rabout(){
-        return "aboutUs" ;
-    }
+   
 }
