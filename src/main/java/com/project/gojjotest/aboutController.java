564a7d389ec2bbb0dfbedb7a626e69fab4404950
muluken <mulukenhakim97@gmail.com>
2022-02-18 12:48:01 +0000

    mule

--- src/main/java/com/project/gojjotest/aboutController.java
+++ src/main/java/com/project/gojjotest/aboutController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class aboutController {
-    @GetMapping("/about")
+    @GetMapping("/aboutUs")
     public String about(){
         return "aboutUs" ;
     }
